5cf09d19f4762ea2c0d9e00dd6bd0886f304cff8
sai Hruthik <sai.hruthick12@gmail.com>
2023-06-26 16:18:49 -0500

    first commit

---
+++ src/samplePlackages/Car.java
@@ -0,0 +1,28 @@
+package samplePlackages;
+
+public class Car {
+
+	private int Model;
+	private String Driver;
+	private int speed;
+	
+	public int getModel() {
+		return Model;
+	}
+	public void setModel(int model) {
+		this.Model = model;
+	}
+	public String getDriver() {
+		return Driver;
+	}
+	public void setDriver(String driver) {
+		this.Driver = driver;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+}
---
+++ src/samplePlackages/Hello.java
@@ -0,0 +1,11 @@
+package samplePlackages;
+
+public class Hello {
+	public static void main(String[] args) {
+		Car c = new Car();
+		c.setSpeed(45);
+		System.out.println(c.getSpeed());		
+	}
+	https://marketplace.eclipse.org/marketplace-client-intro?mpc_install=1794107
+
+}
